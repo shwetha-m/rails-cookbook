de1193563a28461096e77e22ce62e5faa4a87b06
Shwetha Marumada <shwe.nie@gmail.com>
2025-05-12 11:31:20 +0100

    rails-cookbook app models associations and validations implemented

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+  validates :recipe_id, uniqueness: { scope: :category_id, message: 'has already been added to this category' }
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, inclusion: { in: 0..5 }
 end
