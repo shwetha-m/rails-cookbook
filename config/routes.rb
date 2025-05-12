1151bbcb7a3b08d3c5a0676891f71c484f93cae3
Shwetha Marumada <shwe.nie@gmail.com>
2025-05-12 16:06:35 +0100

    Routing, Controller and Views for Bookmarks and made the app navigable

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :categories, only: %i[index show new create]
+  resources :categories, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 
   # get '/categories', to: 'categories#index'
   # get '/categories/:id', to: 'categories#show'
